46839bbe3864e066cfd7523596af3eed6b7066e6
andreibaje <andreibajenaru2012@gmail.com>
2025-03-14 19:26:15 +0100

    Added changes in quantity and name of the objects in main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from product_manager import ProductManager
 
 manager = ProductManager()
 
-product1 = Product("Laptop", 4500, 3)
-product2 = Product("Phone", 3000, 5)
-product3 = Product("Headphones", 500, 10)
+product1 = Product("Gaming Laptop", 4500, 5)
+product2 = Product("SmartPhone", 3000, 7)
+product3 = Product("Wireless Headphones", 500, 13)
 
 manager.add_product(product1)
 manager.add_product(product2)
@@ -14,7 +14,3 @@ manager.add_product(product3)
 print("Available Products:")
 manager.display_products()
 
-print("\nRemoving 'Phone'...")
-manager.remove_product("Phone")
-
-print(f"\nTotal Value: {manager.total_inventory_value()} RON")
