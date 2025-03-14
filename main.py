1558f85bd6128d01bfda6157a901e065d0bfe964
andreibaje <andreibajenaru2012@gmail.com>
2025-03-14 19:16:05 +0100

    Added the shopping cart functionality script and tested in main.py

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
+import random
 
 manager = ProductManager()
+cart = Cart()
 
 product1 = Product("Laptop", 4500, 3)
 product2 = Product("Phone", 3000, 5)
 product3 = Product("Headphones", 500, 10)
+product4 = Product("Mouse", 150, 7)
+product5 = Product("Keyboard", 200, 4)
 
 manager.add_product(product1)
 manager.add_product(product2)
 manager.add_product(product3)
+manager.add_product(product4)
+manager.add_product(product5)
 
 print("Available Products:")
 manager.display_products()
 
-print("\nRemoving 'Phone'...")
-manager.remove_product("Phone")
+random_products = random.sample(manager.products, 3)
+for product in random_products:
+    cart.add_to_cart(product, random.randint(1, product.quantity))
 
-print(f"\nTotal Value: {manager.total_inventory_value()} RON")
+cart.display_cart()
