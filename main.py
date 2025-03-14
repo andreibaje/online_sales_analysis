360b821a5fc1613816e58fb382fce495b67883a3
andreibaje <andreibajenaru2012@gmail.com>
2025-03-14 19:00:02 +0100

    Added the scripts to the dedcated files and tested the code

---
+++ main.py
@@ -0,0 +1,17 @@
+from product import Product
+from product_manager import ProductManager
+
+manager = ProductManager()
+
+product1 = Product("Laptop", 4500, 3)
+product2 = Product("Phone", 3000, 5)
+product3 = Product("Headphones", 500, 10)
+
+manager.add_product(product1)
+manager.add_product(product2)
+manager.add_product(product3)
+
+print("Available Products:")
+manager.display_products()
+
+print(f"\nTotal Value: {manager.total_inventory_value()} RON")
---
+++ product_manager.py
@@ -0,0 +1,17 @@
+from product import Product
+
+class ProductManager:
+    def __init__(self):
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def display_products(self):
+        if not self.products:
+            print("No products available.")
+        for product in self.products:
+            print(product.display_info())
+
+    def total_inventory_value(self):
+        return sum(product.price * product.quantity for product in self.products)
