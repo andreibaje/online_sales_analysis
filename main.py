ba5ba3eaa1dfde0f0c863ae8632d61dbced0038a
andreibaje <andreibajenaru2012@gmail.com>
2025-03-14 19:03:49 +0100

    Added the removal function in product_manager.py and tested it in main.py

--- main.py
+++ main.py
@@ -14,4 +14,7 @@ manager.add_product(product3)
 print("Available Products:")
 manager.display_products()
 
+print("\nRemoving 'Phone'...")
+manager.remove_product("Phone")
+
 print(f"\nTotal Value: {manager.total_inventory_value()} RON")
--- product_manager.py
+++ product_manager.py
@@ -7,6 +7,14 @@ class ProductManager:
     def add_product(self, product):
         self.products.append(product)
 
+    def remove_product(self, product_name):
+        for product in self.products:
+            if product.name.lower() == product_name.lower():
+                self.products.remove(product)
+                print(f"Product '{product_name}' has been removed.")
+                return
+        print(f"Product '{product_name}' not found.")
+
     def display_products(self):
         if not self.products:
             print("No products available.")
